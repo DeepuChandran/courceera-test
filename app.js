4ffc88e31f0064c507d534368ef5559a86a3ba8f
Deepu Chandran <Deepu Chandran>
2023-01-15 22:07:33 +0100

    java script

---
+++ app.js
@@ -0,0 +1,30 @@
+var student = {
+    name : "",
+    type : "student"
+};
+
+document.addEventListener("DOMContentLoaded",contentLoaded)
+
+function contentLoaded(event)
+{
+    document.getElementById("name").addEventListener("keyup",keyup);
+}
+
+function keyup(event)
+{
+    CalculateNumericOutput();
+}
+
+function CalculateNumericOutput()
+{
+    student.name= document.getElementById("name").value;
+
+    var totalnumberofChar= 0 ;
+
+    for(var i=0;i <= student.name.length;i++)
+    {
+        totalnumberofChar = totalnumberofChar + 1
+    }
+
+    document.getElementById("output").value=totalnumberofChar;
+}
